e4f6951188be9a9fa9b14d7e39f9b52ff5883689
Tobias Brodd <tobias.brodd@gmail.com>
2018-11-19 22:59:14 +0100

    refactored existing code and added an optimized tree sequence generator

--- l_system.py
+++ l_system.py
@@ -1,15 +1,35 @@
 import numpy as np
-import time
 from matplotlib import pyplot as plt
 from system import PlantSystem, TreeSystem
 
-def generate_system(rules, system, N=3):
+def generate_sequence(rules, sequence, N=3):
     for i in range(N):
-        new_system = ""
-        for c in system:
-            new_system += rules[c]
-        system = new_system
-    return system
+        new_sequence = ""
+        for c in sequence:
+            new_sequence += rules[c]
+        sequence = new_sequence
+    return sequence
+
+def generate_tree_sequence_optimized(rules, N=3):
+    if N == 0:
+        return "0"
+    left_sequence = "1"
+    right_sequence = "0"
+    sequence = left_sequence + "[0]" + right_sequence
+    if N == 1:
+        return sequence
+    for i in range(N-1):
+        new_left_sequence= ""
+        new_right_sequence = ""
+        for l in left_sequence:
+            new_left_sequence += rules[l]
+        for r in right_sequence:
+            new_right_sequence += rules[r]
+        sequence = new_left_sequence + "[" + sequence + "]" + new_right_sequence
+        left_sequence = new_left_sequence
+        right_sequence = new_right_sequence
+    return sequence
+
 
 def plot_system(system, sequence, x_org=1.0, y_org=0.0, l=1.0, a=90.0):
     stack = []
@@ -34,12 +54,12 @@ def plot_system(system, sequence, x_org=1.0, y_org=0.0, l=1.0, a=90.0):
     return fig
 
 plant_system = PlantSystem()
-plant_sequence = generate_system(plant_system.rules, plant_system.start, N=5)
+plant_sequence = generate_sequence(plant_system.rules, plant_system.start, N=5)
 plant_fig = plot_system(plant_system, plant_sequence)
 plant_fig.savefig("plant.png")
 
 tree_system = TreeSystem()
-tree_sequence = generate_system(tree_system.rules, tree_system.start, N=10)
+tree_sequence = generate_sequence(tree_system.rules, tree_system.start, N=10)
 tree_fig = plot_system(tree_system, tree_sequence)
 plt.show()
 tree_fig.savefig("tree.png")
